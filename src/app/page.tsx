601141bad195230d779fd5d4b25e28de76c6a92e
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2024-02-13 10:13:53 +0600

    add motivation section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "@/components/mainLayout/Banner";
 import Blog from "@/components/mainLayout/Blog";
+import Motivation from "@/components/mainLayout/Motivation";
 
 export default function Home() {
   return (
     <main className="">
       <Banner />
       <Blog />
+      <Motivation />
     </main>
   );
 }
---
+++ src/components/mainLayout/Motivation.tsx
@@ -0,0 +1,23 @@
+import image1 from "../../app/assets/blogimage/0_6LMazODWI5X8MFwU.png";
+import image2 from "../../app/assets/blogimage/banner.jpg";
+
+import Image from "next/image";
+
+function Motivation() {
+  return (
+    <div className="flex justify-center align-middle sm:flex-row flex-col mt-32  sm:gap-x-5">
+      <div className="w-1/2 h-80 relative">
+        <div className="">
+          <Image src={image1} fill alt="" />
+        </div>
+      </div>
+      <div className="w-1/2 h-80 relative">
+        <div className="">
+          <Image src={image2} fill alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Motivation;
