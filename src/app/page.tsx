cc1fac9f3468cf501cddfae2254c400c5d7a9876
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2024-02-13 08:47:38 +0600

    add blog section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Banner from "@/components/mainLayout/Banner";
+import Blog from "@/components/mainLayout/Blog";
 
 export default function Home() {
   return (
     <main className="">
       <Banner />
+      <Blog />
     </main>
   );
 }
