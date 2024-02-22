bf5a578a09b29c443def7f2a3b4c96162554ceeb
Imtiaz653 <shaharukhimtiazsohan@gmail.com>
2024-02-22 21:25:59 +0600

    color updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Subscribe from "@/components/mainLayout/Subscribe";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="border-2 pr-5 pl-5 border-slate-100 shadow-2xl bg-yellow-100 rounded-md mb-5">
       <Banner />
       <Blog />
       <Motivation />
