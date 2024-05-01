2b7f4148eac0cefb5a72dc8ffc0280a0bf50e6ef
mohammadShamimReza <mohammadshamimreza23393@gmail.com>
2024-05-01 21:37:11 +0600

    fix mp3 problem3

--- src/helpers/axios/graphqlBaseQuery.ts
+++ src/helpers/axios/graphqlBaseQuery.ts
@@ -1,15 +1,15 @@
-import { ClientError, request } from "graphql-request";
+// import { ClientError, request } from "graphql-request";
 
-export const graphqlBaseQuery =
-  ({ baseUrl }: { baseUrl: string }) =>
-  async ({ body }: { body: string }) => {
-    try {
-      const result = await request(baseUrl, body);
-      return { data: result };
-    } catch (error) {
-      if (error instanceof ClientError) {
-        return { error: { status: error.response.status, data: error } };
-      }
-      return { error: { status: 500, data: error } };
-    }
-  };
+// export const graphqlBaseQuery =
+//   ({ baseUrl }: { baseUrl: string }) =>
+//   async ({ body }: { body: string }) => {
+//     try {
+//       const result = await request(baseUrl, body);
+//       return { data: result };
+//     } catch (error) {
+//       if (error instanceof ClientError) {
+//         return { error: { status: error.response.status, data: error } };
+//       }
+//       return { error: { status: 500, data: error } };
+//     }
+//   };
