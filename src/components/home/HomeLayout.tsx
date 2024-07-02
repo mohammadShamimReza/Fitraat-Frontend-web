f7d5b4754e88d7a5ffccedcad9fb7359742f2f57
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2024-07-02 13:00:44 +0600

    add new layout

--- src/components/home/HomeLayout.tsx
+++ src/components/home/HomeLayout.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { Button, Layout, Menu, Modal } from "antd";
-import TextArea from "antd/es/input/TextArea";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import { Button, Layout, Menu, theme } from "antd";
+import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import { AiOutlineSave } from "react-icons/ai";
 import { BiTask } from "react-icons/bi";
 import { BsPostcard } from "react-icons/bs";
-import { IoHomeOutline } from "react-icons/io5";
+import { IoCashOutline, IoHomeOutline, IoPersonOutline } from "react-icons/io5";
+import siteLogo from "../../app/assets/detox1.png";
 import FeedPost from "./homefeed/FeedPost";
 
 const { Header, Content, Footer, Sider } = Layout;
@@ -22,7 +23,6 @@ type MenuItem = {
 
 function HomeLayout() {
   const [collapsed, setCollapsed] = useState(false);
-  const [createPostModal, setCreatePostModal] = useState(false);
 
   const authItems: MenuItem[] = [
     {
@@ -32,35 +32,40 @@ function HomeLayout() {
     },
     {
       key: "2",
-      label: <p onClick={() => setCreatePostModal(true)}>Create post</p>,
+      label: <Link href="/blog">Blog</Link>,
       icon: <BsPostcard size={20} />,
     },
     {
       key: "3",
-      label: <Link href="/profile#savedPost">Saved post</Link>,
-      icon: <AiOutlineSave size={20} />,
+      label: <Link href="/myTask">Task</Link>,
+      icon: <BiTask size={20} />,
     },
     {
       key: "4",
-      label: <Link href="/myTask">My Task</Link>,
-      icon: <BiTask size={20} />,
+      label: <Link href="/donation">Donation</Link>,
+      icon: <IoCashOutline size={20} />,
+    },
+    {
+      key: "5",
+      label: <Link href="/profile">Profile</Link>,
+      icon: <IoPersonOutline size={20} />,
     },
   ];
-
-  const handleCreatePost = () => {
-    // Logic for handling post creation
-    console.log("Creating post...");
-  };
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
 
   return (
     <Layout style={{ minHeight: "100vh", borderRadius: "30px" }}>
-      <Sider
-        collapsible
-        collapsed={collapsed}
-        onCollapse={setCollapsed}
-        theme="light"
-      >
+      <Sider theme="light" trigger={null} collapsible collapsed={collapsed}>
         <div className="demo-logo-vertical" />
+        <div className="flex flex-shrink-0 items-center justify-center text-lg font-bold mb-5">
+          <Link href={"/"}>
+            {" "}
+            <Image src={siteLogo} width={70} alt="website logo" />
+          </Link>
+        </div>
+
         <Menu theme="light" defaultSelectedKeys={["1"]} mode="inline">
           {authItems.map((item) => (
             <Item key={item.key} icon={item.icon}>
@@ -70,28 +75,53 @@ function HomeLayout() {
         </Menu>
       </Sider>
       <Layout className="rounded-2xl">
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+          className="border rounded"
+        >
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: "16px",
+              width: 64,
+              height: 64,
+            }}
+          />
+          <Button type="text" shape="round" size="large">
+            <div className="flex items-center justify-center gap-1">
+              <p>Login</p>{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M14 5l7 7m0 0l-7 7m7-7H3"
+                />
+              </svg>
+            </div>
+          </Button>
+        </Header>
         <Content>
           <FeedPost />
         </Content>
         <Footer style={{ textAlign: "center" }}>
-          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+          Save Ummah ©{new Date().getFullYear()} Created by Shamim Reza
         </Footer>
       </Layout>
-      <Modal
-        title="Create Post"
-        visible={createPostModal}
-        onCancel={() => setCreatePostModal(false)}
-        footer={[
-          <Button key="back" onClick={() => setCreatePostModal(false)}>
-            Cancel
-          </Button>,
-          <Button key="submit" type="primary" onClick={handleCreatePost}>
-            Create
-          </Button>,
-        ]}
-      >
-        <TextArea rows={4} placeholder="Enter your post content..." />
-      </Modal>
     </Layout>
   );
 }
