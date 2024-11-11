6c89ee36946d0ea0890d416ea90f0d1d19c2bda0
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2024-11-11 12:45:41 +0600

    adding energency feature

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,7 +1,7 @@
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     // <div className="">
-    <div className="backgroundDot">{children}</div>
+    <div className="backgroundDot min-h-screen">{children}</div>
 
     // </div>
   );
